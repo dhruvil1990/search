3ee44461b049c32cff65ef68c59b7cb87b61734b
Dhruvil Patel <pateldhruvil1990@gmail.com>
2014-05-01 15:52:26 +0530

    1st time

---
+++ test/models/keywords_model_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class KeywordsModelTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/people_model_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PeopleModelTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
